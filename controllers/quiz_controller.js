5282db555b364405e816423d5d25ee408843b9bf
Mario Lobo Herranz <mloboh@gmail.com>
2015-07-20 06:24:53 +0200

    lista preguntas + autoload
    lista de preguntas + autoload.
    todo listo para hacer el ejercicio P2P obligatorio del módulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,36 @@
 var models = require('../models/models.js');
 
-exports.question = function(req, res){
-	models.Quiz.findAll().success(function (quiz) {
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});	
-	});
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if(quiz) {
+				req.quiz = quiz;
+				next();
+			}
+			else {
+				next(new Error("No existe quizId=" + quizId));
+			}
+
+		}).catch(function(error) {next(error);})
+};
+
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function (quizes) {
+		res.render('quizes/index.ejs', {quizes: quizes});	
+	}).catch(function(error) { next(error);});
+};
+
+
+exports.show = function(req, res){
+	res.render('quizes/show', {quiz: req.quiz});	
 };
 
 exports.answer = function(req, res){
-	models.Quiz.findAll().success(function (quiz) {
-		if(req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer', {respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
-		}
-	});
+	var resultado = "Incorrecto";
+	if(req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	} 
+	res.render('quizes/answer', {quiz: quiz, respuesta: resultado});
 };
 
 exports.author = function(req, res){
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author',quizController.author);
 
 module.exports = router;
